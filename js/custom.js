4fb26688ac1d8fde9ef62691dc73227dc63f4aba
jujut1998 <junninhothomas1998@gmail.com>
2016-07-15 10:36:00 -0400

    secondary commit

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,3 @@
-/*
-  * @package 
-  * @subpackage template name HTML
-  * 
-  * Template Scripts
-  * Created by themeturn
-  
-  1. Fixed header
-  2. Site search
-  3. Main slideshow
-  4. Owl Carousel
-      a. Testimonial
-      b. Clients
-      c. Team
-  5. Back to top
-  6. Skills
-  7. BX slider
-      a. Blog Slider
-      b. Portfolio item slider
-  8. Isotope
-  9. Animation (wow)
-  10. Flickr
-  
-*/
-
-
 jQuery(function($) {
   "use strict";
 
@@ -38,20 +12,20 @@ jQuery(function($) {
     });
 
     $.noConflict();
-     $('.nav a').on('click', function(){ 
+     $('.nav a').on('click', function(){
         if($('.navbar-toggle').css('display') !='none'){
             $(".navbar-toggle").trigger( "click" );
         }
     });
 
 
- 
+
 // prettyphoto
 
  $("a[data-rel^='prettyPhoto']").prettyPhoto();
- 
-    
- 
+
+
+
 
 
 
